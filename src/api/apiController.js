a5ed6caa8d5618dbcc7a6ca8bb950792a5124b8f
unknown <you@example.com>
2022-02-05 16:00:46 -0300

    Again trying cookies issue with cross origin

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -45,7 +45,7 @@ module.exports = class controller {
 
                         const loginToken = createTokens(user);
                         // res.status(200).json({"login-token": loginToken});
-                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24}, { sameSite: 'none', secure: true })
+                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24, SameSite: 'none', secure: true})
                         res.status(200).json({message: "Logged in!"});
                     } else {
                         res.status(400).json({error: "Wrong username and/or password."});
