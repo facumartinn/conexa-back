95f0acefcca4cf6e3cab50f79f90a31c4e54ce28
unknown <you@example.com>
2022-02-05 14:46:10 -0300

    Cookies

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -45,7 +45,8 @@ module.exports = class controller {
 
                         const loginToken = createTokens(user);
                         // res.status(200).json({"login-token": loginToken});
-                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24, httpOnly: false})
+                        res.setHeader('Access-Control-Allow-Origin', 'https://conexa-challenge.netlify.app');
+                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24, secure: true})
                         res.status(200).json({message: "Logged in!"});
                     } else {
                         res.status(400).json({error: "Wrong username and/or password."});
