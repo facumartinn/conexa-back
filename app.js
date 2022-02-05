d6b6b239eae88d0694d4334819df8064a9489db1
unknown <you@example.com>
2022-02-05 15:07:48 -0300

    cookies

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 
-
+app.set("trust proxy", 1);
 app.use(cors({ origin: true, credentials: true }));
 // app.options('*', cors())
 app.use(logger('dev'));
