0b657e35f42dc344eed808dae9dd2e9994bb4a8c
unknown <you@example.com>
2022-02-04 22:27:37 -0300

    Heroku deploy

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ const app = express();
 
 
 app.use(cors({ origin: true, credentials: true }))
-app.options('*', cors())
+// app.options('*', cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 // Routes
