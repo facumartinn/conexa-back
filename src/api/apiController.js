b4d21f945f0945641d2b86771a18ee10d1254bb3
unknown <you@example.com>
2022-02-05 15:31:55 -0300

    testing cookies for API

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -45,7 +45,7 @@ module.exports = class controller {
 
                         const loginToken = createTokens(user);
                         // res.status(200).json({"login-token": loginToken});
-                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24, secure: true})
+                        res.cookie("logintoken", loginToken, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true, secure: true, SameSite: 'none'})
                         res.status(200).json({message: "Logged in!"});
                     } else {
                         res.status(400).json({error: "Wrong username and/or password."});
